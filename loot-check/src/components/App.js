03cb1b2697851f8f0f93ae75b0b1425b33db2e27
Luke Fiji <luke.fiji@gmail.com>
2017-10-27 23:05:54 -0700

    Set up loot check

--- loot-check/src/components/App.js
+++ loot-check/src/components/App.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        <h2>Loot Check</h2>
+      </div>
+    );
   }
 }
 
